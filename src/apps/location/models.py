21b5a5d1bcb979982a5a4b958982cd343b93dc76
anton.danilchenko <anton.danilchenko@localhost>
2011-01-21 18:43:23 +0200

    Changed apps.location: changed models

--- src/apps/location/models.py
+++ src/apps/location/models.py
@@ -11,7 +11,7 @@ class Currency(db.Model):
 class Country(db.Model):
     '''Country'''
     KEY_NAME = "%(slug)s"
-    slug     = db.SlugProperty("Short country form (us, uk, fr)", required=True, max_length=50)
+    slug     = db.SlugProperty("short country form (us, uk, fr)", required=True, max_length=50)
     name     = db.StringProperty(required=True, max_length=100)
     # main currency used in this country
     currency = db.ReferenceProperty(Currency)
@@ -22,21 +22,27 @@ class Country(db.Model):
 class Region(db.Model):
     '''Region (state)'''
     KEY_NAME = "%(slug)s"
-    slug     = db.SlugProperty("Short state form (kiev.ua, msk.ru, nw.us)", required=True, max_length=50)
+    slug     = db.SlugProperty("short state form (kiev, msk, ny)", required=True, max_length=50)
     name     = db.StringProperty(required=True, max_length=100)
     country  = db.ReferenceProperty(reference_class=Country, required=True)
 
     def __unicode__(self):
+        return self.name
+
+    def full_name(self):
         return "%s, %s" % (self.name, self.country)
 
 class City(db.Model):
     '''City'''
     KEY_NAME = "%(slug)s"
     UNIQUE   = ("phone_code", ("name", "region"))
-    slug     = db.SlugProperty("Short city form (kiev.ua, msk.ru, nw.us)", required=True, max_length=50)
+    slug     = db.SlugProperty("short city form (kiev, msk, ny)", required=True, max_length=50)
     name     = db.StringProperty(required=True, max_length=100)
     region   = db.ReferenceProperty(reference_class=Region, required=True)
     phone_code  = db.StringProperty(max_length=6)
 
     def __unicode__(self):
+        return self.name
+
+    def full_name(self):
         return "%s (%s)" % (self.name, self.region)
