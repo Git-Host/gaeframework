e2c5246699e305fb653572a2fba4f0b2e26f0cf9
anton.danilchenko <anton.danilchenko@localhost>
2011-02-03 09:36:33 +0200

    Improvement in gae.forms: now we can pass initial values to form constructor. and this value (if not editable in form) to be automatically setted to model instance

--- src/gae/forms/__init__.py
+++ src/gae/forms/__init__.py
@@ -15,7 +15,7 @@ class ModelForm(djangoforms.ModelForm):
     '''
     Added: automatically translate fields and choices in model.
     '''
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         '''Added i18m support to model fields and choices'''
         # detect current application name by model name
         app_name = re.sub("^([A-Z][^A-Z]+).*", "\\1", self.__class__.__name__).lower()
@@ -28,4 +28,14 @@ class ModelForm(djangoforms.ModelForm):
             # translate choices
             if hasattr(field, "widget") and hasattr(field.widget, "choices"):
                 field.widget.choices = [(key, _(name, app_name)) for key, name in field.widget.choices]
-        super(ModelForm, self).__init__(**kwargs)
+        super(ModelForm, self).__init__(*args, **kwargs)
+
+    def _clean_fields(self):
+        '''Fix for Django 1.2: use initial data'''
+        # set additional properties to model, not editable in form
+        if hasattr(self, "initial"):
+            self.cleaned_data = {}
+            for field_name, value in self.initial.items():
+                if field_name not in self.base_fields.keys(): # field not editable in form
+                    self.cleaned_data[field_name] = value
+        super(ModelForm, self)._clean_fields()
