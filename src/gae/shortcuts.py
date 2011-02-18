33d7c8cc901b71165a993916ca8d82f5ba89c0b6
anton.danilchenko <anton.danilchenko@localhost>
2011-02-19 01:20:18 +0200

    Improved "get_object_or_404" to work with named keys, ids and real keys in one place

--- src/gae/shortcuts.py
+++ src/gae/shortcuts.py
@@ -2,7 +2,7 @@ from gae import webapp
 
 def get_object_or_404(model, key_name):
     '''Return object with given key or show "page not found"'''
-    obj = model.get_by_key_name(key_name)
+    obj = model.get_by_key_name(key_name) or model.get_by_id(int(key_name)) or model.get(key_name)
     if not obj:
         webapp.instance.error(404)
         raise Exception("Object '%s' with key '%s' not found" %
