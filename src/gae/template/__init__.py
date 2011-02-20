d6f1bb290870a961175a8a40a75daf89fed5d805
anton.danilchenko <anton.danilchenko@localhost>
2011-02-20 22:40:42 +0200

    Added standard template loader for compatibility with appengine administrative interface

--- src/gae/template/__init__.py
+++ src/gae/template/__init__.py
@@ -9,7 +9,7 @@ try:
     django.conf.settings.configure(
         DEBUG = debug,
         TEMPLATE_DEBUG = debug,
-        TEMPLATE_LOADERS=('gae.template.loader.Loader',),
+        TEMPLATE_LOADERS=('gae.template.loader.Loader', 'django.template.loaders.filesystem.load_template_source',),
         LANGUAGE_CODE = get_config("site.language"),
         USE_I18N = True,
         USE_L10N = True,
