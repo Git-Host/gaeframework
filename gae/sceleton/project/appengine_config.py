cd6869281401d9ffc00c793c9130ad1716ddce01
anton.danilchenko <anton.danilchenko@localhost>
2011-06-25 14:14:12 +0300

    (fixed) Bug with set session key if application name is very short

--- gae/sceleton/project/appengine_config.py
+++ gae/sceleton/project/appengine_config.py
@@ -3,7 +3,7 @@ from google.appengine.ext.appstats import recording
 from gae.sessions import SessionMiddleware
 import gae.webapp
 
-COOKIE_KEY = 'my_private_key_used_for_site_%s' % os.environ['APPLICATION_ID']
+COOKIE_KEY = 'my_private_key_used_for_this_site_%s' % os.environ['APPLICATION_ID']
 
 def webapp_add_wsgi_middleware(app):
     app = SessionMiddleware(app, cookie_key=COOKIE_KEY, cookie_only_threshold=0)
