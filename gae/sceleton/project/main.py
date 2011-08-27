56f1ab1581bc1a382e8606707221920cec1f1b08
anton.danilchenko <anton.danilchenko@localhost>
2011-08-27 16:53:07 +0300

    (changed) File mainpy was changed to set search path in current project

--- gae/sceleton/project/main.py
+++ gae/sceleton/project/main.py
@@ -1,5 +1,8 @@
 from gae.webapp import run
-import os
+import os, sys
 
 if __name__ == "__main__":
-    run(os.path.dirname(__file__))
+    project_path = os.path.dirname(__file__)
+    if project_path not in sys.path:
+        sys.path.insert(0, project_path)
+    run(project_path)
