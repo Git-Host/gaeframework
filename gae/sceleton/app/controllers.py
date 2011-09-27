eb5dcb9af7e0e99c78cb987a582708c08363255d
anton <anton@localhost>
2011-09-28 00:26:36 +0300

    (changed) Added useful instruction for each new application. (fixed) Render page for each new application

--- gae/sceleton/app/controllers.py
+++ gae/sceleton/app/controllers.py
@@ -1,3 +1,18 @@
 
 def index(request):
-    return "[app_name]/index.html"
+    '''
+    Render main page.
+
+    Template located in "apps/[app_name]/templates/index.html"
+    
+    You can also do:
+      * show plain text with
+        return request.text("Hello World!")
+
+      * show data representation in XML
+        return request.xml({'city': 'California', 'ccountry': 'USA'})
+
+      * show data representation in JSON
+        return request.json({'city': 'California', 'ccountry': 'USA'})
+    '''
+    return request.render("[app_name]/index")
